a36f15fbe8b2b560b4cd70774bc16acde3d8596a
Adelodun Damilare <adelodundamilare@gmail.com>
2024-03-10 03:01:02 +0100

    some fixes

--- src/components/static/RightSide/Pdf/MainContent/OCRPdfContent.jsx
+++ src/components/static/RightSide/Pdf/MainContent/OCRPdfContent.jsx
@@ -1,122 +1,183 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Left from '../../../LeftSide/Left'
-import BackIcon from '../../../../../assets/back.svg'
-import { GiHamburgerMenu } from 'react-icons/gi'
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { authRequest } from "../../../../../config/baseUrl";
+import Left from "../../../LeftSide/Left";
+import BackIcon from "../../../../../assets/back.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { FaCopy } from "react-icons/fa";
-import DeviceIcon from '../../../../../assets/device.svg'
-import DropBoxIcon from '../../../../../assets/dropbox.svg'
-import { AiOutlinePlus } from 'react-icons/ai'
-import { ImCross } from 'react-icons/im'
+import ProgressModal from "../../../../Progress";
+import { Document, Page } from "react-pdf";
+import DeviceIcon from "../../../../../assets/device.svg";
+import DropBoxIcon from "../../../../../assets/dropbox.svg";
+import { AiOutlinePlus } from "react-icons/ai";
+import { ImCross } from "react-icons/im";
+import Message from "./DownloadPDF/Message";
 
 const OCRPdfContent = () => {
+  const [showSideBar, setshowSideBar] = useState(false);
+  const [showLeftSideBar, setshowLeftSideBar] = useState(false);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [fileUrl, setFileUrl] = useState(null);
+  const [input, setInput] = useState("");
+  const [mergeID, setMergeId] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const location = useLocation();
+  const nav = useNavigate();
+  const pdfFile = location.state.pdf[0];
+  const handler = async (e) => {
+    const formData = new FormData();
+    formData.append("input_pdf", location.state.pdf[0]);
 
-    const [showSideBar, setshowSideBar] = useState(false)
-    const [showLeftSideBar, setshowLeftSideBar] = useState(false)
-    const nav = useNavigate()
+    try {
+      setIsLoading(true);
+      const response = await authRequest.post("/pdf/ocr_to_pdf/", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
-    return (
-        <div className='relative'>
+      const fileUrl = response.data.data.pdf;
+      setFileUrl(fileUrl);
 
-            {/* HUMBURGER MENU  */}
-            <div className='ml-3 md:hidden block pt-3'>
-                <GiHamburgerMenu className='text-2xl cursor-pointer' onClick={() => setshowSideBar(!showSideBar)} />
-            </div>
+      setIsButtonClicked(true);
+      toast.success("OCR to PDF conversion successful");
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error?.message);
+      console.error("Error converting files:", error);
+    }
+  };
 
-            <div className='flex justify-between h-[100vh] overflow-y-auto overflow-x-hidden font-roboto'>
+  if (isLoading) return <ProgressModal />;
 
-                <div className='flex-1 relative'>
+  return (
+    <>
+      {isButtonClicked ? (
+        <Message
+          mergeID={mergeID}
+          fileUrl={fileUrl}
+          onClose={() => setIsButtonClicked(false)}
+        />
+      ) : (
+        <div className="relative">
+          {/* HUMBURGER MENU  */}
+          <div className="ml-3 md:hidden block pt-3">
+            <GiHamburgerMenu
+              className="text-2xl cursor-pointer"
+              onClick={() => setshowSideBar(!showSideBar)}
+            />
+          </div>
 
-                    <div className='flex gap-3 cursor-pointer p-3 h-fit' onClick={() => nav("/pdf/OCR")}>
-                        <img src={BackIcon} alt="" />
-                        <p>Back</p>
-                    </div>
+          <div className="flex justify-between h-[100vh] overflow-y-auto overflow-x-hidden font-roboto">
+            <div className="flex-1 relative">
+              <div
+                className="flex gap-3 cursor-pointer p-3 h-fit"
+                onClick={() => nav("/pdf/OCR")}
+              >
+                <img src={BackIcon} alt="" />
+                <p>Back</p>
+              </div>
 
-                    <div className='absolute right-2'>
-
-                        <div className='w-[2rem] h-[2rem] rounded-[0.375rem] bg-[#20808d] flex justify-center items-center mb-2 cursor-pointer'>
-                            <AiOutlinePlus alt="" className=' text-white' />
-                        </div>
-
-                        <div className='w-[2rem] h-[2rem] rounded-[0.375rem] bg-[#20808d] flex justify-center items-center mb-2 cursor-pointer'>
-                            <FaCopy alt="" className=' text-white' />
-                        </div>
-
-                        <div className='w-[2rem] h-[2rem] rounded-[0.375rem] bg-[#20808d] flex justify-center items-center mb-2 cursor-pointer'>
-                            <img src={DeviceIcon} alt="" />
-                        </div>
-
-                        <div className='w-[2rem] h-[2rem] rounded-[0.375rem] bg-[#20808d] flex justify-center items-center mb-2 cursor-pointer'>
-                            <img src={DropBoxIcon} alt="" />
-                        </div>
-
-                    </div>
-
-                    <div className='flex justify-center items-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-
-                        <div className='w-[15rem] rounded-[0.5rem] bg-gray-100'>
-                            <p className='text-sm m-4 bg-white p-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt ut quod dicta cum numquam adipisci rem odit. Corrupti sed quidem id esse beatae dolor facere eaque cupiditate, recusandae, perferendis placeat in impedit obcaecati, voluptatum ullam temporibus fugit. Error unde sapiente illo maxime cupiditate, porro exercitationem suscipit, amet quasi iste ut.</p>
-                        </div>
-                    </div>
-
-
+              <div className="absolute right-2">
+                <div className="w-[2rem] h-[2rem] rounded-[0.375rem] bg-[#20808d] flex justify-center items-center mb-2 cursor-pointer">
+                  <AiOutlinePlus alt="" className=" text-white" />
                 </div>
 
-
-                <div className='w-[13rem] hidden  shadow-CardShadow md:flex justify-start items-center flex-col pt-4 pb-4 relative'>
-
-                    <div className='flex justify-center items-center flex-col'>
-                        <p className='text-lg text-center pl-4 pr-4'>OCR Your Pdf</p>
-                    </div>
-
-
-
-                    <div className=' absolute bottom-3 flex justify-center items-center'>
-                        <button className='bg-[#20808D] text-white w-[10rem] h-[2.5rem] rounded-md'>OCR</button>
-                    </div>
-
+                <div className="w-[2rem] h-[2rem] rounded-[0.375rem] bg-[#20808d] flex justify-center items-center mb-2 cursor-pointer">
+                  <FaCopy alt="" className=" text-white" />
                 </div>
 
-                {
-                    !showLeftSideBar && (
-                        <div className='md:hidden block p-3 absolute top-0 right-0' onClick={() => setshowLeftSideBar(true)}>
-                            <GiHamburgerMenu className='text-2xl cursor-pointer' onClick={() => setshowLeftSideBar(true)} />
-                        </div>
-                    )
-                }
+                <div className="w-[2rem] h-[2rem] rounded-[0.375rem] bg-[#20808d] flex justify-center items-center mb-2 cursor-pointer">
+                  <img src={DeviceIcon} alt="" />
+                </div>
 
-                {
-                    // reponsive side bar 
-                    showLeftSideBar && (
+                <div className="w-[2rem] h-[2rem] rounded-[0.375rem] bg-[#20808d] flex justify-center items-center mb-2 cursor-pointer">
+                  <img src={DropBoxIcon} alt="" />
+                </div>
+              </div>
 
-                        <div className='w-[13rem] h-[100vh] bg-white md:hidden shadow-CardShadow absolute top-0 right-0 p-4 flex justify-start items-center flex-col'>
-
-                            <div className='w-[100%] mb-4 cursor-pointer' onClick={() => setshowLeftSideBar(false)}>
-                                <ImCross className='float-right' onClick={() => setshowLeftSideBar(false)} />
-                            </div>
-
-                            <div className='flex justify-center items-center flex-col'>
-                                <p className='text-lg text-center pl-4 pr-4'>OCR Your PDF</p>
-                            </div>
-
-                            <div className=' absolute bottom-3 flex justify-center items-center'>
-                                <button className='bg-[#20808D] text-white w-[10rem] h-[2.5rem] rounded-md'>OCR PDF</button>
-                            </div>
-
-
-                        </div>
-                    )
-                }
-
+              <div className="flex justify-center items-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                <div className="w-[15rem] rounded-[0.5rem] bg-gray-100">
+                  <p className="text-sm m-4 bg-white p-3">
+                    <Document
+                      file={location.state.pdf[0]}
+                      onLoadError={(error) =>
+                        console.error("Error loading document:", error)
+                      }
+                    >
+                      <Page
+                        pageNumber={1}
+                        renderTextLayer={false}
+                        renderAnnotationLayer={false}
+                      />
+                    </Document>
+                  </p>
+                  <span
+                    style={{
+                      display: "block",
+                      textAlign: "center",
+                      margin: "0 auto",
+                    }}
+                    className="font-bold"
+                  >
+                    {pdfFile.name}
+                  </span>
+                </div>
+              </div>
             </div>
+
+            <div className="w-[13rem] hidden  shadow-CardShadow md:flex justify-start items-center flex-col pt-4 pb-4 relative">
+              <div className=" absolute bottom-3 flex justify-center items-center">
+                <button
+                  onClick={handler}
+                  className="bg-[#20808D] text-white w-[10rem] h-[2.5rem] rounded-md"
+                >
+                  Convert
+                </button>
+              </div>
+            </div>
+
+            {!showLeftSideBar && (
+              <div
+                className="md:hidden block p-3 absolute top-0 right-0"
+                onClick={() => setshowLeftSideBar(true)}
+              >
+                <GiHamburgerMenu
+                  className="text-2xl cursor-pointer"
+                  onClick={() => setshowLeftSideBar(true)}
+                />
+              </div>
+            )}
 
             {
-                showSideBar && (
-                    <div className='lg:hidden block absolute top-0 left-0'><Left showSideBar={showSideBar} setshowSideBar={setshowSideBar} /></div>
-                )
+              // reponsive side bar
+              showLeftSideBar && (
+                <div className="w-[13rem] h-[100vh] bg-white md:hidden shadow-CardShadow absolute top-0 right-0 p-4 flex justify-start items-center flex-col">
+                  <div
+                    className="w-[100%] mb-4 cursor-pointer"
+                    onClick={() => setshowLeftSideBar(false)}
+                  >
+                    <ImCross
+                      className="float-right"
+                      onClick={() => setshowLeftSideBar(false)}
+                    />
+                  </div>
+                </div>
+              )
             }
-        </div>
-    )
-}
+          </div>
 
-export default OCRPdfContent
+          {showSideBar && (
+            <div className="lg:hidden block absolute top-0 left-0">
+              <Left showSideBar={showSideBar} setshowSideBar={setshowSideBar} />
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default OCRPdfContent;
