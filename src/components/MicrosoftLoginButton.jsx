a3ce93fa133fd8881e89f71c5c974f8b548247a8
Adelodun Damilare <adelodundamilare@gmail.com>
2024-04-17 14:33:49 +0100

    fixes

--- src/components/MicrosoftLoginButton.jsx
+++ src/components/MicrosoftLoginButton.jsx
@@ -1,54 +1,45 @@
 import React, { useState, useRef } from "react";
-import MicrosoftLogin from 'react-microsoft-login';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom'
+import MicrosoftLogin from "react-microsoft-login";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const MicrosoftLoginButton = () => {
-
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [msalInstance, onMsalInstanceChange] = useState();
-  const YOUR_CLIENT_ID = '7ef6bc24-cbd7-4e47-b874-e9c417da13d6'; // Replace with your actual Microsoft client ID
-  const YOUR_CLIENT_SECRET  = '-Y48Q~Z.xujVkRw8GqEuxy-p5b5kENybY~mJDcj2'
-//   const url = 'http://localhost:5173/login-route';
-// parasApp;0c9544f9-5424-4e23-b742-bbe5879e0416
-// chatgpt:0ffbd016-485a-463c-a606-537c0852954f
-// DjangoAppSSO:7ef6bc24-cbd7-4e47-b874-e9c417da13d6
-// client_secret:-Y48Q~Z.xujVkRw8GqEuxy-p5b5kENybY~mJDcj2
+  const YOUR_CLIENT_ID = import.meta.env.VITE_MICROSOFT_LOGIN_CLIENT_ID; // Replace with your actual Microsoft client ID
+  const YOUR_CLIENT_SECRET = import.meta.env.VITE_MICROSOFT_LOGIN_CLIENT_SECRET;
+  //   const url = 'http://localhost:5173/login-route';
+
   // const url = 'http://localhost:5173';
-  const url = 'https://ai-lawyer.neuracase.com';
-  
+  const url = "https://ai-lawyer.neuracase.com";
 
-
-  
-
-  const loginHandler =  async (err, response, data, msal) => {
+  const loginHandler = async (err, response, data, msal) => {
     console.log("working");
 
-    console.log(response, )
-    console.log(err, 'err')
+    console.log(response);
+    console.log(err, "err");
 
-   
     // navigate('/dashboard');
 
     // // Some actions
     if (!err && response) {
       const authorizationCode = response.accessToken;
-      const tokensResponse = await axios.post('http://127.0.0.1:8000/accounts/dj-rest-auth/microsoft/login/', {
-        access_token: response.accessToken,
-        id_token: response.idToken,
-      });
+      const tokensResponse = await axios.post(
+        "http://127.0.0.1:8000/accounts/dj-rest-auth/microsoft/login/",
+        {
+          access_token: response.accessToken,
+          id_token: response.idToken,
+        }
+      );
 
-    //   const tokens = tokensResponse.data;
+      //   const tokens = tokensResponse.data;
       const token = tokensResponse.data.key;
-      console.log('Access Tokens:', token);
-      localStorage.setItem('token', token);
+      console.log("Access Tokens:", token);
+      localStorage.setItem("token", token);
       onMsalInstanceChange(msal);
-      navigate('/dashboard');
+      navigate("/dashboard");
     }
   };
-
-
-
 
   return (
     <div>
@@ -57,17 +48,10 @@ const MicrosoftLoginButton = () => {
         authCallback={loginHandler}
         redirectUri={url}
         // client_secret = {YOUR_CLIENT_SECRET}
-        
       >
         Continue with Microsoft
       </MicrosoftLogin>
-
-
-      </div>
-
-
-
-    
+    </div>
   );
 };
 
