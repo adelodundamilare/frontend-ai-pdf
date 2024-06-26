9ac9592d96a37b17ae8a01a4be28613c2df29332
Adelodun Damilare <adelodundamilare@gmail.com>
2024-06-26 12:35:48 +0100

    add delete upload

--- src/components/dynamic/Pdf/UploadAndDownloadPdf.tsx
+++ src/components/dynamic/Pdf/UploadAndDownloadPdf.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { CiEdit, CiTrash } from "react-icons/ci";
+import { toast } from "react-toastify";
 //
 import { authRequest } from "@/config/baseUrl";
 import ProgressModal from "@/components/Progress";
@@ -17,8 +18,8 @@ interface Prop {
 }
 const UploadAndDownloadPdf = ({ title }: Prop) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingModal, setIsLoadingModal] = useState(false);
 
-  const arr = [1, 2, 3, 4, 5];
   const [showPopup, setshowPopup] = useState<{ show: boolean; id: any }>({
     show: false,
     id: null,
@@ -53,9 +54,41 @@ const UploadAndDownloadPdf = ({ title }: Prop) => {
       const response = await authRequest.get("/history/user_uploads/");
       setData(response.data.data);
     } catch (error) {
-      console.error("Error fetching user profile:", error);
+      console.error("Error", error);
     } finally {
       setIsLoading(false);
+    }
+  };
+
+  const renameUpload = async (item: any) => {
+    try {
+      setIsLoading(true);
+      const response = await authRequest.post(`/history/rename_upload/`, {
+        name: "name new",
+        id: item.id,
+      });
+    } catch (error) {
+      console.error("Error", error);
+      toast.error(`Unable to update File, please try again`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const deleteUpload = async (item: any) => {
+    try {
+      setIsLoadingModal(true);
+      const response = await authRequest.delete(
+        `/history/delete_upload/${item?.id}/`
+      );
+      if (response) {
+        await fetchData();
+      }
+    } catch (error) {
+      console.error("Error", error);
+      toast.error(`Unable to delete File, please try again`);
+    } finally {
+      setIsLoadingModal(false);
     }
   };
 
@@ -94,7 +127,7 @@ const UploadAndDownloadPdf = ({ title }: Prop) => {
 
         {data && data.length === 0 && (
           <div className="flex justify-center items-center h-[20rem]">
-            <h1>No data found</h1>
+            <h1>No record found</h1>
           </div>
         )}
 
@@ -141,24 +174,26 @@ const UploadAndDownloadPdf = ({ title }: Prop) => {
                         </p>
                       </div>
 
-                      {/* {showPopup.show === true && showPopup.id === item ? (
+                      {showPopup.show === true && showPopup.id === item ? (
                         <div className="bg-[#D9D9D9] absolute top-10 h-[7rem] right-2 w-[10rem] rounded-lg flex justify-center items-center flex-col">
-                          <div
+                          {/* <button
+                            onClick={() => renameUpload(item)}
                             style={{ border: "1px solid #D9D9D9" }}
                             className="flex w-[90%] cursor-pointer p-2 items-center justify-between bg-[#F5f5f5] rounded-lg"
                           >
                             <p>Rename</p>
                             <CiEdit />
-                          </div>
-                          <div
+                          </button> */}
+                          <button
+                            onClick={() => deleteUpload(item)}
                             style={{ border: "1px solid #D9D9D9" }}
                             className="flex w-[90%] cursor-pointer p-2 items-center justify-between bg-[#F5f5f5] rounded-lg mt-1"
                           >
                             <p>Delete</p>
                             <CiTrash />
-                          </div>
+                          </button>
                         </div>
-                      ) : null} */}
+                      ) : null}
                     </div>
                   );
                 })}
@@ -193,24 +228,26 @@ const UploadAndDownloadPdf = ({ title }: Prop) => {
                     >
                       <img src={DotImage} alt="" />
                     </div>
-                    {/* {showPopup.show === true && showPopup.id === item ? (
+                    {showPopup.show === true && showPopup.id === item ? (
                       <div className="bg-[#D9D9D9] absolute top-0 h-[6rem] right-2 w-[10rem] rounded-lg flex justify-center items-center flex-col">
-                        <div
+                        {/* <button
+                          onClick={() => renameUpload(item?.file ?? "")}
                           style={{ border: "1px solid #D9D9D9" }}
                           className="flex w-[90%] cursor-pointer p-1 pl-2 pr-2 items-center justify-between bg-[#F5f5f5] rounded-lg"
                         >
                           <p>Rename</p>
                           <CiEdit />
-                        </div>
-                        <div
+                        </button> */}
+                        <button
+                          onClick={() => deleteUpload(item)}
                           style={{ border: "1px solid #D9D9D9" }}
                           className="flex w-[90%] cursor-pointer p-1 pl-2 pr-2 items-center justify-between bg-[#F5f5f5] rounded-lg mt-1"
                         >
                           <p>Delete</p>
                           <CiTrash />
-                        </div>
+                        </button>
                       </div>
-                    ) : null} */}
+                    ) : null}
                   </div>
                 );
               })}
